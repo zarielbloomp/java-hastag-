0cee496e1f1e53c2ecf0146e8409ff0d57e6cfed
zarielbloomp <miguel.c.nicoli@gmail.com>
2024-02-01 10:54:47 -0300

    fiz o caradpio

--- cardapio digital/hashtaurant/src/App.jsx
+++ cardapio digital/hashtaurant/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Hashtaurant from "./assets/hashtaurante.webp";
+import './App.css';
+import { Navegação } from "./Navegação";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export function App() {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas]
+  const [paginaSelecionada, atualizarPaginaSelecionada] =useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img src={Hashtaurant} alt="" className="capa"/>
+      <Navegação atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+      <div className="menu">
+        {paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>) }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
-
-export default App
